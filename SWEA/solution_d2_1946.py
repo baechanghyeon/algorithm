4dbad8f9298fe707a56ea9965fa02eab9b3b44ad
Changhyeon Bae <pek5302@naver.com>
2022-05-15 23:38:38 +0900

    d2

--- SWEA/solution_d2_1946.py
+++ SWEA/solution_d2_1946.py
@@ -1,27 +1,14 @@
-# t = int(input())
-#
-# for tc in range(t):
-#     r_str = int(input())
-#     str_list = ""
-#
-#     for i in range(1, r_str+1):
-#         a, b = input().split()
-#         str_list += a*int(b)
-#     cnt = len(str_list)
-#     if cnt - 10 < 0:
-#         cnt -= 10
-#         str_list.insert()
-#
-#
-
-T = int(input())
-
-for tc in range(1, T+1):
-    N = int(input())
-    text = ''
-    for n in range(N):
-        alhpa, num = input().split()
-        text += alhpa*int(num)
-    print('#{}'.format(tc))
-    for i in range(1,len(text)+1,10):
-        print(text[i-1:i+10-1])
+t = int(input())
+for tc in range(1,t+1):
+    alpha = int(input())
+    result =""
+    #문자 갯수 만큼 문자열에 더하기
+    for x in range(alpha):
+        a, b = input().split()
+        result += a * int(b)
+    # 10칸마다 줄바꿈
+    point=""
+    for i in range(len(result)//10+1):
+        point += result[i*10:(i+1)*10] + "\n"
+    delete = point[:-1]
+    print(f'#{tc}\n{delete}')
