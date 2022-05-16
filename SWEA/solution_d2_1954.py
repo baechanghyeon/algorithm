2327dbc33e857f7280fa8e56e13297f1f2c7e75a
Changhyeon Bae <pek5302@naver.com>
2022-05-17 01:22:06 +0900

    d2

--- SWEA/solution_d2_1954.py
+++ SWEA/solution_d2_1954.py
@@ -1,35 +1,73 @@
-T = int(input())
+# T = int(input())
+#
+# # row, col 인덱스로 탐색할 수 있게 방향 설정 (달팽이 방향이니까 우->하->좌->상)
+# dr = [0, 1, 0, -1]
+# dc = [1, 0, -1, 0]
+#
+# for tc in range(1, T+1):
+#     N = int(input())
+#     snail = [[0]*N for _ in range(N)]
+#     # 초기 위치 & 회전방향 설정
+#     r, c = 0, 0
+#     dist = 0  # 0:우, 1:하, 2:좌, 3:상
+#
+#     for n in range(1, N*N + 1):
+#         snail[r][c] = n
+#         r += dr[dist]
+#         c += dc[dist]
+#
+#         # 범위를 벗어나거나 0이 아닌 다른 값이 이미 있다면, dist 방향 체인지
+#         # 근데 이런 경우라면 요소 인덱스를 다시 원위치시켜줘야하므로 빼줘야함
+#         # 그런다음 dist의 방향을 바꾸고, 방향 바꿨으면 다시 움직일 수 있도록 행렬 인덱스 업데이트
+#         if r < 0 or c < 0 or r >= N or c >= N or snail[r][c] != 0:
+#             # 실행취소
+#             r -= dr[dist]
+#             c -= dc[dist]
+#             # dist는 % 4 안해주면 0123, 4567, ... 이런식으로 숫자 커지므로 나머지로 접근
+#             dist = (dist + 1) % 4
+#             #  다시 gogo
+#             r += dr[dist]
+#             c += dc[dist]
+#
+#     print("#{}".format(tc))
+#     for row in snail:
+#         print(*row)
+#     print()
 
-# row, col 인덱스로 탐색할 수 있게 방향 설정 (달팽이 방향이니까 우->하->좌->상)
-dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0]
+T= int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    snail = [[0]*N for _ in range(N)]
-    # 초기 위치 & 회전방향 설정
-    r, c = 0, 0
-    dist = 0  # 0:우, 1:하, 2:좌, 3:상
+    # N * N 의 빈 리스트 생성
+    snail = [[0] * N for _ in range(N)]
 
-    for n in range(1, N*N + 1):
-        snail[r][c] = n
-        r += dr[dist]
-        c += dc[dist]
+    #방향 전환
+    # 우 하 좌 상
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
 
-        # 범위를 벗어나거나 0이 아닌 다른 값이 이미 있다면, dist 방향 체인지
-        # 근데 이런 경우라면 요소 인덱스를 다시 원위치시켜줘야하므로 빼줘야함
-        # 그런다음 dist의 방향을 바꾸고, 방향 바꿨으면 다시 움직일 수 있도록 행렬 인덱스 업데이트
-        if r < 0 or c < 0 or r >= N or c >= N or snail[r][c] != 0:
-            # 실행취소
-            r -= dr[dist]
-            c -= dc[dist]
-            # dist는 % 4 안해주면 0123, 4567, ... 이런식으로 숫자 커지므로 나머지로 접근
-            dist = (dist + 1) % 4
-            #  다시 gogo
-            r += dr[dist]
-            c += dc[dist]
+    dist = 0 # 0:우, 1:하, 2:좌:, 3:사에0
+    #초기 좌표
+    x = y = 0
+    snail[x][y] = 1
 
-    print("#{}".format(tc))
-    for row in snail:
-        print(*row)
-    print()
+    for num in range(2, N**2+1):
+        x += dx[dist]
+        y += dy[dist]
+        #배열에 숫자 채우기
+        snail[x][y] = num
+        # 인덱스가 범위 안에 있고, 숫자가 아직 안써졌다면 같은 모드 유지
+        if 0 <= x+dx[dist] < N and 0 <= y + dy[dist] < N and not snail[x+dx[dist][y+dy[dist]]]:
+            continue
+        # 아니라면 dist 변경
+        if dist != 3:
+            dist += 1
+        else:
+            dist = 0
+
+    print(f'#{tc}')
+    for i in snail:
+        print(*i)
+
+
+
