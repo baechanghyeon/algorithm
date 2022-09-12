4ac4bd8b4b0aa876d4e0c260c6bbec634c7d9196
baechanghyeon <pek5302@naver.com>
2022-09-12 17:22:09 +0900

    BinarySearch Example2

--- 이코테/음료수얼려먹기.py
+++ 이코테/음료수얼려먹기.py
@@ -1,22 +1,5 @@
-n, m = map(int, input().split())
-
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-# 모든 노드 (위치)에 대하여 음료수 채우기
-result = 0
-for i in range(n):
-    for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            result += 1
-
-print(result)
 
 # DFS로 특정 노드를 방문하고 연결된  모든 노드들도 방문
-
-
 def dfs(x, y):
     # 주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
@@ -33,3 +16,20 @@ def dfs(x, y):
         dfs(x, y + 1)
         return True
     return False
+
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 모든 노드 (위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
