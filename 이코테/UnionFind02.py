385696cb50141be3f72b440af7d6352c1aa8ac81
baechanghyeon <pek5302@naver.com>
2022-09-25 23:30:44 +0900

    UnionFind example2

---
+++ 이코테/UnionFind02.py
@@ -0,0 +1,8 @@
+# 찾기 함수를 재귀적으로 호출 한뒤에 부모 테이블 값을 바로 갱신
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x) :
+  # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+  if parent[x] != x :
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
