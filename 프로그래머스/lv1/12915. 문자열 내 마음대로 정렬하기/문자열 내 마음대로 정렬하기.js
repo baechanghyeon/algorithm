a32404f5d39f1cef9c9d0516b32f54348ca43fb2
ChangHyeon Bae <pek5302@naver.com>
2023-03-14 23:55:41 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.42 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,21 @@
+// strings = 문자열로 구성된 리스트
+// n = 정수 
+function solution(strings, n) {
+    const a = []
+    strings.forEach((i, idx) => {
+        // 해당 index 알파벳 찾기
+        a[idx] = [i[n], i]
+
+    })
+    // 정렬하기
+    const result = [...a].sort((a,b) => {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    })
+    // 값 도출
+    const answer = []
+    result.forEach((i, idx) => {
+       answer.push(i[1])
+    })
+    return answer  
+}
