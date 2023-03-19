cdef45555a4026ff2979df9da8b5aade2cd850f1
ChangHyeon Bae <pek5302@naver.com>
2023-03-19 21:27:00 +0900

    [level 2] Title: 기능개발, Time: 2.26 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,29 @@
+// 앞선 프로젝트가 완료되야 이후에 프로젝트도 완성이 됨.
+function solution(progresses, speeds) {
+    const result = []
+    for(let i=0; i< progresses.length; i++) {
+        let day = 0
+        // 각 배포날짜 구하기
+        while(progresses[i] < 100) {
+            progresses[i] += speeds[i]
+            day++
+        }
+        result.push(day)
+    }
+    const answer = []
+    let work = result[0]
+    let cnt = 1
+    for(let i=1; i<result.length; i++) {
+        console.log(work, result[i], cnt)
+        if(work >= result[i]) cnt++;
+        else if(work < result[i]){
+            work = result[i]
+            answer.push(cnt)
+            cnt = 1
+        }      
+    }
+    answer.push(cnt)
+    return answer
+}
+    
+ 
