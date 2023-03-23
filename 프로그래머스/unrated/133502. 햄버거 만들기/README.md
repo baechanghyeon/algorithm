7f6e4a89081b78574fd8114aeaa80a0a85b8be76
ChangHyeon Bae <pek5302@naver.com>
2023-03-23 23:25:00 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/README.md
+++ 프로그래머스/unrated/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 2.47 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -3,24 +3,9 @@ function solution(ingredient) {
     for(let i = 0; i < ingredient.length-3; i++) {
         if(ingredient[i] === 1 && ingredient[i+1] === 2 && ingredient[i+2] === 3 && ingredient[i+3]===1){
             ingredient.splice(i,4)
-            i -=4
+            i -=3
             cnt++
         }
     }    
-    console.log(ingredient)
     return cnt
-    
-    // # 2
-    // let cnt = 0
-    // let i = 0
-    // while(i < ingredient.length-3) {
-    //     if(ingredient[i] === 1 && ingredient[i+1] === 2 && ingredient[i+2] === 3 && ingredient[i+3]) {
-    //         cnt++
-    //         ingredient.splice(i,4)
-    //         i=0
-    //     } else {
-    //         i++;
-    //     }
-    // }
-    // return cnt
 }
