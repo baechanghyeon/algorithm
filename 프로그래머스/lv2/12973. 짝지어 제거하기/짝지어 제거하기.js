b629ca6e8a32a27eb69ce5c06fa3216ed6c848a8
ChangHyeon Bae <pek5302@naver.com>
2023-03-28 21:44:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 38.03 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,6 +1,6 @@
 function solution(s){
   let answer = []
-
+  
     for(let i of s) {
        answer.push(i)
        if(answer[answer.length-1] === answer[answer.length-2]){
@@ -8,6 +8,5 @@ function solution(s){
            answer.pop()
        }
     }
-    if(answer.length === 0) return 1
-    else return 0
+    return answer.length ? 0 : 1
 }
