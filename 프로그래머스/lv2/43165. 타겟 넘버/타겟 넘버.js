4cc55b9a46b84db81bb98cefbd4e52eb96bfe5d8
ChangHyeon Bae <pek5302@naver.com>
2023-04-04 16:59:23 +0900

    [level 2] Title: 타겟 넘버, Time: 1.07 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -2,17 +2,12 @@
 // 더한값이 마이너스 인 경우 => false
 function solution(numbers, target) {
     let answer = 0
-    function func(idx, hap) {
-        if(idx===numbers.length) {
-            if(hap === target) answer++
-            return;
+    for(let i=0; i<numbers.length; i++) {
+        let arr = []
+        for(let j=0; j<numbers.length; j++) {
+            if(j===i) arr.push(-numbers[j])
+            else arr.push(numbers[j])
         }
-        
-        // 더하거나 빼는 쪽으로 빠져야함. 트리형태로 퍼져나감.
-        func(idx+1, hap-numbers[idx])
-        func(idx+1, hap+numbers[idx])
-    }
-    
-    func(0, 0) // 0번째 숫자부터 시작하고, 시작값 합계 0을 파라미터로 전달
-    return answer
+        console.log(arr)
+    } 
 }  
